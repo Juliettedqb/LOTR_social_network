4b49ffb7e8c36275ee1f4e29cc9bdf0634d045e6
Theo Seuge <theoseuge@neey.local>
2023-02-15 09:59:18 +0100

    add message part on wall.php

--- Niveau1/wall.php
+++ Niveau1/wall.php
@@ -1,3 +1,13 @@
+<?php
+    session_start();
+    $idU = $_SESSION['connected_id'];
+    if (!isset($idU)) {
+        header("Location: login.php");
+        exit();
+    } else {
+        echo $idU;
+    }
+?>
 <!doctype html>
 <html lang="fr">
     <head>
@@ -11,16 +21,16 @@
             <img src="resoc.jpg" alt="Logo de notre réseau social"/>
             <nav id="menu">
                 <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
+                <a href="wall.php?user_id=<?php echo $idU ?>">Mur</a>
+                <a href="feed.php?user_id=<?php echo $idU ?>">Flux</a>
                 <a href="tags.php?tag_id=1">Mots-clés</a>
             </nav>
             <nav id="user">
                 <a href="#">Profil</a>
                 <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                    <li><a href="settings.php?user_id=<?php echo $idU ?>">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=<?php echo $idU ?>">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=<?php echo $idU ?>">Mes abonnements</a></li>
                 </ul>
 
             </nav>
@@ -38,7 +48,7 @@
             ?>
             <?php
             /**
-             * Etape 2: se connecter à la base de donnée
+             * Etape 2: se idUer à la base de donnée
              */
             include("fonctions.php");
             ?>
@@ -63,6 +73,59 @@
                 </section>
             </aside>
             <main>
+            <article>
+                    <h2>Poster un message</h2>
+                    <?php
+                    /**
+                     * BDj
+                     */
+                    $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                    /**
+                     * TRAITEMENT DU FORMULAIRE
+                     */
+                    // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
+                    // si on recoit un champs email rempli il y a une chance que ce soit un traitement
+                    // on ne fait ce qui suit que si un formulaire a été soumis.
+
+                    // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
+                    // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
+                    /* echo "<pre>" . print_r($_POST, 1) . "</pre>"; */
+                    // et complétez le code ci dessous en remplaçant les ???
+                    $authorId = $idU;
+                    $postContent = $_POST['message'];
+                    $enCoursDeTraitement = isset($postContent);
+                    if ($enCoursDeTraitement) {
+                        //Etape 3 : Petite sécurité
+                        // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                        $authorId = intval($mysqli->real_escape_string($authorId));
+                        $postContent = $mysqli->real_escape_string($postContent);
+                        //Etape 4 : construction de la requete
+                        $lInstructionSql = "INSERT INTO posts "
+                                . "(id, user_id, content, created, parent_id) "
+                                . "VALUES (NULL, "
+                                . $authorId . ", "
+                                . "'" . $postContent . "', "
+                                . "NOW(), "
+                                . "NULL);"
+                                ;
+                        /* echo $lInstructionSql; */
+                        // Etape 5 : execution
+                        $ok = $mysqli->query($lInstructionSql);
+                        if ( ! $ok) {
+                            echo "Impossible d'ajouter le message: " . $mysqli->error;
+                        } else {
+                            echo "Message posté en tant que :" . $listAuteurs[$authorId];
+                        }
+                    }
+                    ?>                     
+                    <form action="" method="post">
+                        <input type='hidden' name='???' value='achanger'>
+                        <dl><label for='message'>Message</label></dt>
+                            <dd><textarea name='message'></textarea></dd>
+                        </dl>
+                        <input type='submit'>
+                    </form>               
+                </article>
                 <?php
                 /**
                  * Etape 3: récupérer tous les messages de l'utilisatrice
